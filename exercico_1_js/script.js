5fd785e171a89093ed8e80b7d3da26da296685dc
edwin241996 <67443862+edwin241996@users.noreply.github.com>
2020-07-24 17:47:34 -0400

    Add files via upload

---
+++ exercico_1_js/script.js
@@ -0,0 +1,9 @@
+alert('olá, voce quer fazer um teste rapido emtre dois pessoa')
+
+alert('vamos ala, sera divertido')
+
+let nome1 = prompt("nome1")
+let idade1 = prompt("idade1")
+
+let nome2 = prompt("nome2")
+let idade2 = prompt("idade2")
